7b166850048772736bd85afad80f7c2d564edbf6
shin-fkmt <fkmt.shin@gmail.com>
2026-08-20 16:38:33 +0900

    本の一覧画面にページング機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :books
+  concern :paginatable do
+    get '(page/:page)', action: :index, on: :collection, as: ''
+  end
+  resources :books, concerns: :paginatable
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
