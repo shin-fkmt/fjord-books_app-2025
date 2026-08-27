4f0327c8fd799e114c51a592af243ec2eca51ad1
shin-fkmt <fkmt.shin@gmail.com>
2026-08-27 15:34:57 +0900

    URLのクエリストリングでページ指定を行うよう修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  concern :paginatable do
-    get '(page/:page)', action: :index, on: :collection, as: ''
-  end
-  resources :books, concerns: :paginatable
+  resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
